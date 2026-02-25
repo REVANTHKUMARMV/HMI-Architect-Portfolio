ca39caa96a0779df49d4f6f88f7de1a4d5f90c60
Revanth Kumar <revanthkumar.skht@gmail.com>
2026-02-25 15:22:05 +0100

    hal_sim.h

--- projects/centrifuge-ui-demo/src/hal_sim.h
+++ projects/centrifuge-ui-demo/src/hal_sim.h
@@ -1,1 +1,27 @@
+#pragma once
+#include "hal_interface.h"
+#include <QTimer>
 
+class HalSim : public HalInterface {
+    Q_OBJECT
+public:
+    explicit HalSim(QObject* parent = nullptr);
+
+public slots:
+    void start();
+    void stop();
+    void setMotorEnabled(bool on) override;
+    void setTargetRpm(int rpm) override;
+    void clearFault() override;
+
+private slots:
+    void onTick();
+
+private:
+    QTimer _tick;
+    bool _enabled{false};
+    int  _targetRpm{0};
+    int  _currentRpm{0};
+    bool _fault{false};
+    const int _maxSafeRpm{15000};
+};
