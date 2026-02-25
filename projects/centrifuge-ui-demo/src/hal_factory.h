ed66e43cd4ad8fd54f11f23ae7ccd43ebf00c63d
Revanth Kumar <revanthkumar.skht@gmail.com>
2026-02-25 15:23:27 +0100

    hal_factory.h

--- projects/centrifuge-ui-demo/src/hal_factory.h
+++ projects/centrifuge-ui-demo/src/hal_factory.h
@@ -1,1 +1,23 @@
+#pragma once
+#include <QObject>
+#include <QThread>
 
+class HalInterface;
+
+class HalFactory : public QObject {
+    Q_OBJECT
+public:
+    static HalFactory& instance();
+    HalInterface* hal() const { return _hal; }
+
+    void start();
+    void stop();
+
+private:
+    explicit HalFactory(QObject* parent = nullptr);
+    ~HalFactory();
+
+    QThread _thread;
+    HalInterface* _hal{nullptr};
+    bool _running{false};
+};
