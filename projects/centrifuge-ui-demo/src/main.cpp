df2f23a682ee1f31e20505d68477419c39069e6e
Revanth Kumar <revanthkumar.skht@gmail.com>
2026-02-25 15:26:32 +0100

    main.cpp

--- projects/centrifuge-ui-demo/src/main.cpp
+++ projects/centrifuge-ui-demo/src/main.cpp
@@ -1,1 +1,28 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+#include <QQmlContext>
 
+#include "rpm_viewmodel.h"
+#include "centrifuge_controller.h"
+#include "hal_factory.h"
+#include "hal_interface.h"
+
+int main(int argc, char *argv[]) {
+    QGuiApplication app(argc, argv);
+
+    HalFactory::instance().start();
+    HalInterface* hal = HalFactory::instance().hal();
+
+    CentrifugeController controller(hal);
+    RpmViewModel vm(&controller);
+
+    QQmlApplicationEngine engine;
+    engine.rootContext()->setContextProperty("rpmVM", &vm);
+
+    const QUrl url(u"qrc:/Centrifuge/qml/Main.qml"_qs);
+    engine.load(url);
+
+    const int rc = app.exec();
+    HalFactory::instance().stop();
+    return rc;
+}
