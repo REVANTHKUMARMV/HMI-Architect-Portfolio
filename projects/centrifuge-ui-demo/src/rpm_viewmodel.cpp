2ea38d829c2eac475820f7a93d94b45d8e83d198
Revanth Kumar <revanthkumar.skht@gmail.com>
2026-02-25 15:25:54 +0100

    rpm_viewmodel.cpp

--- projects/centrifuge-ui-demo/src/rpm_viewmodel.cpp
+++ projects/centrifuge-ui-demo/src/rpm_viewmodel.cpp
@@ -1,1 +1,54 @@
+#include "rpm_viewmodel.h"
+#include "centrifuge_controller.h"
 
+RpmViewModel::RpmViewModel(CentrifugeController* controller, QObject* parent)
+    : QObject(parent), _controller(controller)
+{
+    connect(_controller, &CentrifugeController::currentRpmUpdated,
+            this, &RpmViewModel::onBackendRpm);
+
+    connect(_controller, &CentrifugeController::targetRpmChanged,
+            this, &RpmViewModel::onBackendTarget);
+
+    connect(_controller, &CentrifugeController::stateChanged,
+            this, &RpmViewModel::onBackendState);
+
+    connect(_controller, &CentrifugeController::faultActive,
+            this, &RpmViewModel::onBackendFault);
+}
+
+void RpmViewModel::cmdStart(int rpm) {
+    _controller->start(rpm);
+}
+
+void RpmViewModel::cmdStop() {
+    _controller->stop();
+}
+
+void RpmViewModel::cmdClearFault() {
+    _controller->clearFault();
+}
+
+void RpmViewModel::onBackendRpm(int rpm) {
+    if (_currentRpm == rpm) return;
+    _currentRpm = rpm;
+    emit currentRpmChanged();
+}
+
+void RpmViewModel::onBackendTarget(int rpm) {
+    if (_targetRpm == rpm) return;
+    _targetRpm = rpm;
+    emit targetRpmChanged();
+}
+
+void RpmViewModel::onBackendState(CentrifugeState s) {
+    if (_state == s) return;
+    _state = s;
+    emit stateChanged();
+}
+
+void RpmViewModel::onBackendFault(bool on) {
+    if (_fault == on) return;
+    _fault = on;
+    emit faultChanged();
+}
